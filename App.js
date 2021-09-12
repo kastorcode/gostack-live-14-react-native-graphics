da6d4cbbdaf7cca0f2b1159c38fc51dbb37e88fb
Matheus <ramalholiveira13@gmail.com>
2021-09-12 09:27:48 -0300

    First commit.

---
+++ App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Area from './src/Area';
+import Barra from './src/Barra';
+import Linha from './src/Linha';
+import Pizza from './src/Pizza';
+
+const App = () => {
+  return (
+    <>
+      <Area />
+    </>
+  );
+};
+
+export default App;
